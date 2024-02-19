aa77faa593b5a202eb43709e61502a80470177a4
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-19 20:00:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import {TextGenerateEffect} from '../components/heroText';
 export default function Home() {
   return (
     <>
-    
-    <TextGenerateEffect words="And here is your app, sleek, fast and trusted. Attendance, reminder, Day order and marks password protected" className='text-center pt-[57px]'/>
+    <div className='h-[45vh] flex items-end'>
+    <TextGenerateEffect words="And here is your app, sleek, fast and trusted. Attendance, reminder, Day order and marks password protected" className='text-center pl-5 pr-5'/>
+    </div>
 
     </>
   );
