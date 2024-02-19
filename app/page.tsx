08aa0bc4f232582ebe2d5c2a18a2c0b6e231a7a7
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-19 19:48:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import {FloatingNav} from '../components/navbar';
 import {TextGenerateEffect} from '../components/heroText';
 
 export default function Home() {
   return (
     <>
-    <FloatingNav navItems={[
-      { name: "Home", link: "/" },
-      { name: "Attendance", link: "/attendance" },
-      { name: "TimeTable", link: "/timetable" },
-    ]}/>
+    
     <TextGenerateEffect words="And here is your app, sleek, fast and trusted. Attendance, reminder, Day order and marks password protected" className='text-center pt-[57px]'/>
 
     </>
