427ca5b787232439a25301f98481c75f6278ca62
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-20 03:45:07 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -75,10 +75,11 @@ export const FloatingNav = ({
             <span className="block text-sm">{navItem.name}</span>
           </Link>
         ))}
+        <Link href='/login'>
         <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
           <span>Login</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button>
+        </button></Link>
       </motion.div>
     </AnimatePresence>
   );
