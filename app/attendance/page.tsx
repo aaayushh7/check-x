daa02183545e7b029a16df05d7a6b68a4d51bf77
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-21 03:34:17 +0530

    Update page.tsx

--- app/attendance/page.tsx
+++ app/attendance/page.tsx
@@ -1,24 +1,20 @@
-// import {TextGenerateEffect} from '../components/heroText';
-import {HoverEffect} from '@/components/attendance'
-import { title } from 'process';
-// import {FloatingNav} from '../components/navbar';
-
+import { HoverEffect } from '@/components/attendance'
+import { Attendance } from '@/data/attendance'
 
 export default function Home() {
   return (
-    <>
-    
-    <div className='pt-[15%] pr-5 pl-5'>
-    <HoverEffect items={[
-      {
-        title:"Mathematics",
-        description:'Mat padho, number to vaise bhi ni ane!',
-        marks: '75%',
-        link:'Nhi milega',
-      }
-    ]}/>
-    </div>
+    <div className='pt-[10%] pl-5 pr-5'>
 
-    </>
+<HoverEffect items={Attendance
+  .filter(item => item.key !== '') // Filter out items with empty keys
+  .map((item, idx) => ({
+    title: item.subject,
+    description: '',
+    marks: item.attendance,
+    link: '',
+    key: item.index,
+}))} />
+
+</div>
   );
 }
