1967bd26fdae3eb946586c02a03ede6af3492914
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-20 21:35:18 +0530

    Update runpuppeteer.ts

--- api/runpuppeteer.ts
+++ api/runpuppeteer.ts
@@ -3,7 +3,7 @@ import { Login } from "@/scraper/login";
 export default async function handler(email:string,pass:string) {
     try {
        await Login(email,pass);
-        console.log("scap called") 
+        console.log("Custom Login api called.") 
         
     } catch (error) {
         console.error('Error executing Login:', error);
