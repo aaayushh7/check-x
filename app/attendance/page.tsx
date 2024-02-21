02d31d7004037b62e5ebf910ccd1208d7c72a399
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-21 20:59:32 +0530

    Update page.tsx

--- app/attendance/page.tsx
+++ app/attendance/page.tsx
@@ -1,20 +1,22 @@
+"use client"
 import { HoverEffect } from '@/components/attendance'
 import { Attendance } from '@/data/attendance'
 
 export default function Home() {
+
   return (
     <div className='pt-[10%] pl-5 pr-5'>
 
-<HoverEffect items={Attendance
-  .filter(item => item.key !== '') // Filter out items with empty keys
-  .map((item, idx) => ({
-    title: item.subject,
-    description: '',
-    marks: item.attendance,
-    link: '',
-    key: item.index,
-}))} />
+      <HoverEffect items={Attendance
+        .filter(item => item.key !== '') // Filter out items with empty keys
+        .map((item, idx) => ({
+          title: item.code,
+          description: item.subject,
+          marks: item.attendance,
+          link: '',
+          key: item.index,
+        }))} />
 
-</div>
+    </div>
   );
 }
