6ad529aba0aeb1dfb51111ab05c5b341f8c7241e
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-21 20:59:24 +0530

    Update login.ts

--- scraper/login.ts
+++ scraper/login.ts
@@ -1,11 +1,13 @@
 "use server"
 import { getBrowserInstance } from '../utils/browserManager';
 import { getAttendance } from './attendance';
+import { getSlots } from './slot';
 
 const url = 'https://academia.srmist.edu.in/';
 
 
 export async function Login(email:string,pass:string) {
+    
     try {
         const browser = await getBrowserInstance();
 
@@ -19,12 +21,14 @@ export async function Login(email:string,pass:string) {
         await new Promise(resolve => setTimeout(resolve, 1000));
         await frame?.type('#password_container .textbox_div #password', pass);
         await frame?.click('#nextbtn');
-        await new Promise(resolve => setTimeout(resolve, 5000));
+        await new Promise(resolve => setTimeout(resolve, 1000));
         console.log("Login succesful!")
         // await browser.close(); 
         await getAttendance();
-        console.log('getAttendance')
+        await getSlots();
+        // console.log('getAttendance')
         await browser.close();
+        // return browser;
     }
     catch(err){
         console.log(err);
