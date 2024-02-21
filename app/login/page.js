163d21c70e6ccadd7c36ec297648315f98450817
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-22 02:59:27 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,6 @@
-// Assuming you are using React 17 or newer, you don't need to import React just for JSX.
-// But if you see an error related to JSX not recognized, uncomment the next line.
-// import React from 'react';
 "use client"
 import { useState } from 'react';
+import React from "react";
 
 const LoginPage = () => {
     const [email, setEmail] = useState('');
@@ -56,8 +54,13 @@ const LoginPage = () => {
                         <input type="password" value={password} onChange={handlePasswordChange} className="block w-52 py-2 px-2 text-sm text-stone-300 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-slate-200 focus:border-blue-500 peer" placeholder="Password" />
                     </div>
                     <div className="w-full flex justify-center items-center pt-7">
-                        <button type="submit" disabled={loading} className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block">
-                            {loading ? 'Logging in...' : 'Login'}
+                        <button type="submit" disabled={loading} className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+                            <span className="absolute inset-0 overflow-hidden rounded-full">
+                                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+                            </span>
+                            <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                                {loading ? 'Logging in...' : 'Login'}
+                            </div>
                         </button>
                     </div>
                 </form>
