d761acb47026bd04adb13090c964a8dc1494b6dc
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-21 20:59:08 +0530

    Create runAttendance.ts

---
+++ api/runAttendance.ts
@@ -0,0 +1,12 @@
+
+import { getAttendance } from "@/scraper/attendance";
+export default async function handlerAttendance() {
+    try {
+       await getAttendance();
+        console.log("Attendance called")
+        console.log("saved to data.js") 
+        
+    } catch (error) {
+        console.error('Error executing Login:', error);
+    }
+}
